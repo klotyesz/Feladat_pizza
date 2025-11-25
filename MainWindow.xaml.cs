8b8e277539c6b86596a553a49a73f05308d005f0
klotyesz <ifj.sarkadi.g@gmail.com>
2025-11-25 13:09:41 +0100

    Added ListBox & ComboBox

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,9 +16,27 @@ namespace Feladat_pizza
     /// </summary>
     public partial class MainWindow : Window
     {
+        string pizzateszta = "";
+        string pizzaMeret = "";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void lb_pizzateszta_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(lb_pizzateszta.SelectedItem is ListBoxItem item)
+            {
+                pizzateszta = item.Content.ToString();
+            }
+        }
+
+        private void combobox_meret_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lb_pizzateszta.SelectedItem is ComboBoxItem item)
+            {
+                pizzaMeret = item.Content.ToString();
+            }
         }
     }
 }
